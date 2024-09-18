426d5bebe599b5c6eb819fa1b64a0aca06572cd6
Greg Knoll <gregory@bccn-berlin.de>
2024-09-18 15:42:40 +0200

    Can now loop over multiple sessions if they are in metadata.
    This distinguishes a new session, defined in
    session_params.py:read_metadata() from old sessions.
    
    A new session reads the metadata and the whole pipeline will have
    to be run (minus LFP maybe).
    
    If something changes, maybe only a part of the pipeline needs to
    be run, for instance the aligned traces and data object creation.
    In that case, the new block will load the metadata for all
    sessions in the lists and loop over the pipeline.

--- post_spike_sort/post_cluster_pipeline.py
+++ post_spike_sort/post_cluster_pipeline.py
@@ -23,7 +23,8 @@ from neuropixels_preprocessing.session_params import *
 # The information in the metadata block of session_params needs to be
 # filled out and updated for each recording session.
 # ---------------------------------------------------------------------- #
-DATA_ROOT = 'Y:'  # ['local', 'server', 'X:', etc.]
+NEW_SESSION = False
+DATA_ROOT = 'local'  # ['local', 'server', 'X:', etc.]
 SPIKES_AND_TTL = True
 BEHAVIOR = True
 LFPs = False
@@ -37,135 +38,147 @@ WRITE_METADATA = True
 # ---------------------------------------------------------------------- #
 #                           PATHS
 # ---------------------------------------------------------------------- #
-metadata = read_metadata()
-session_paths = get_session_path(metadata, DATA_ROOT, is_ephys_session=True)
-preprocess_dir = session_paths['preprocess_dir']
-ou.make_dir_if_nonexistent(preprocess_dir)
-rec_dir = session_paths['rec_dir']
-behavior_mat_file = session_paths['behav_dir'] + metadata['behavior_mat_file']
-# ---------------------------------------------------------------------- #
+if NEW_SESSION:
+    metadata_l = [read_metadata()]
+else:  # Used for rerunning parts or all of the pipeline on existing sessions
+    DATA_DIR = get_root_path(DATA_ROOT)
+    metadata_l = []
+    RB_rat_name_l = ['TQ02', 'TQ02', 'TQ03', 'TQ03', 'Nina2']
+    RB_date_l = ['20210505', '20210506', '20210615', '20210616', '20210625']
+    for rat, session in zip(RB_rat_name_l, RB_date_l):
+        metadata_l.append(load_session_metadata_from_csv(data_root=DATA_ROOT, rat=rat, session_date=session))
+
+for metadata in metadata_l:
+    interpolation_param_dict['trial_event_interpolation_lengths'][4] = \
+        int(1.0 * sps) if metadata['task'] == 'reward-bias' else int(3.0 * sps)
+    session_paths = get_session_path(metadata, DATA_ROOT, is_ephys_session=True)
+    preprocess_dir = session_paths['preprocess_dir']
+    ou.make_dir_if_nonexistent(preprocess_dir)
+    rec_dir = session_paths['rec_dir']
+    behavior_mat_file = session_paths['behav_dir'] + metadata['behavior_mat_file']
+    # ---------------------------------------------------------------------- #
 
 
-# ---------------------------------------------------------------------- #
-#                           PIPELINE                                     #
-# ---------------------------------------------------------------------- #
-if SPIKES_AND_TTL:
-    for probe_i in range(1, metadata['n_probes']+1):
-        metadata['probe_num'] = probe_i
+    # ---------------------------------------------------------------------- #
+    #                           PIPELINE                                     #
+    # ---------------------------------------------------------------------- #
+    if SPIKES_AND_TTL:
+        for probe_i in range(1, metadata['n_probes']+1):
+            metadata['probe_num'] = probe_i
 
+            print('---------------------------------------------------------------------------------')
+            # load/create ephys-specific (probe-specific) paths
+            spike_dir = preprocess_dir + f'probe{probe_i}/'
+            ou.make_dir_if_nonexistent(spike_dir)
+            if SAVE_INDIVIDUAL_SPIKETRAINS:
+                ou.make_dir_if_nonexistent(spike_dir + 'spike_times/')
+
+            # process ephys recordings
+            tu.create_spike_mat(session_paths['probe_dir'].format(probe_i), spike_dir, session_paths['timestamps_dat'],
+                                metadata, fs, save_individual_spiketrains=SAVE_INDIVIDUAL_SPIKETRAINS)
+
+        tu.find_recording_gaps(session_paths['timestamps_dat'], fs, max_ISI, preprocess_dir + gap_filename)
+
+        if metadata['ott_lab']:
+            tu.extract_TTL_trial_start_times(session_paths['probe_dir'], gap_filename, metadata['DIO_port_num'], save_dir=preprocess_dir)
+            tu.reconcile_TTL_and_behav_trial_start_times(rec_dir, preprocess_dir, behavior_mat_file)
+        else:
+            tu.convert_TTL_timestamps_to_nbit_events(rec_dir, gap_filename, save_dir=preprocess_dir)
+            tu.add_TTL_trial_start_times_to_behav_data(rec_dir, preprocess_dir, behavior_mat_file, gap_filename)
+
+
+    if BEHAVIOR:
         print('---------------------------------------------------------------------------------')
-        # load/create ephys-specific (probe-specific) paths
-        spike_dir = preprocess_dir + f'probe{probe_i}/'
-        ou.make_dir_if_nonexistent(spike_dir)
-        if SAVE_INDIVIDUAL_SPIKETRAINS:
-            ou.make_dir_if_nonexistent(spike_dir + 'spike_times/')
-
-        # process ephys recordings
-        tu.create_spike_mat(session_paths['probe_dir'].format(probe_i), spike_dir, session_paths['timestamps_dat'],
-                            metadata, fs, save_individual_spiketrains=SAVE_INDIVIDUAL_SPIKETRAINS)
-
-    tu.find_recording_gaps(session_paths['timestamps_dat'], fs, max_ISI, preprocess_dir + gap_filename)
-
-    if metadata['ott_lab']:
-        tu.extract_TTL_trial_start_times(session_paths['probe_dir'], gap_filename, metadata['DIO_port_num'], save_dir=preprocess_dir)
-        tu.reconcile_TTL_and_behav_trial_start_times(rec_dir, preprocess_dir, behavior_mat_file)
-    else:
-        tu.convert_TTL_timestamps_to_nbit_events(rec_dir, gap_filename, save_dir=preprocess_dir)
-        tu.add_TTL_trial_start_times_to_behav_data(rec_dir, preprocess_dir, behavior_mat_file, gap_filename)
+        print('Processing behavioral data...')
+        bu.create_behavioral_dataframe(preprocess_dir, metadata)
+        print('Done.')
 
 
-if BEHAVIOR:
-    print('---------------------------------------------------------------------------------')
-    print('Processing behavioral data...')
-    bu.create_behavioral_dataframe(preprocess_dir, metadata)
-    print('Done.')
-
-
-if LFPs:
-    # -------------------------------------------------------- #
-    #               Find LFPs for a given session
-    #             (Common directory for both probes)
-    # -------------------------------------------------------- #
-    LFP_dir = session_paths['rec_dir']+metadata['trodes_datetime']+'.LFP/'
-    assert os.path.exists(LFP_dir)
-    LFP_dat_l = np.sort(os.listdir(LFP_dir))
-    LFP_file_str = LFP_dat_l[0].split('nt')[0] + 'nt' + '{}' + 'ch1.dat'
-
-    for probe_i in range(1, metadata['n_probes']+1):
+    if LFPs:
         # -------------------------------------------------------- #
-        #              Probe-specific output directory
+        #               Find LFPs for a given session
+        #             (Common directory for both probes)
         # -------------------------------------------------------- #
-        metadata['probe_num'] = probe_i
-        session_paths = get_session_path(metadata, DATA_ROOT, is_ephys_session=True)
-        lfp_output_dir = preprocess_dir + f'probe{probe_i}/'
-        # lfp_output_dir = preprocess_dir + f'probe{probe_i}/' + 'LFPs/'
-        # ou.make_dir_if_nonexistent(lfp_output_dir)
+        LFP_dir = session_paths['rec_dir']+metadata['trodes_datetime']+'.LFP/'
+        assert os.path.exists(LFP_dir)
+        LFP_dat_l = np.sort(os.listdir(LFP_dir))
+        LFP_file_str = LFP_dat_l[0].split('nt')[0] + 'nt' + '{}' + 'ch1.dat'
 
-        # -------------------------------------------------------- #
-        #      Export LFP data from channels of good units
-        # -------------------------------------------------------- #
-        cluster_label_df = pd.read_csv(session_paths['probe_dir'] + 'cluster_info.tsv', sep="\t")
-        good_clusters = cluster_label_df.cluster_id[cluster_label_df['group'] == 'good'].to_numpy()
-        good_cluster_channels = cluster_label_df.ch[cluster_label_df['group'] == 'good'].to_numpy()
-        assert len(good_clusters) == len(good_cluster_channels)
+        for probe_i in range(1, metadata['n_probes']+1):
+            # -------------------------------------------------------- #
+            #              Probe-specific output directory
+            # -------------------------------------------------------- #
+            metadata['probe_num'] = probe_i
+            session_paths = get_session_path(metadata, DATA_ROOT, is_ephys_session=True)
+            lfp_output_dir = preprocess_dir + f'probe{probe_i}/'
+            # lfp_output_dir = preprocess_dir + f'probe{probe_i}/' + 'LFPs/'
+            # ou.make_dir_if_nonexistent(lfp_output_dir)
 
-        choice_df = bu.select_choice_trials_w_TTLs(joblib.load(preprocess_dir + 'behav_df'))
-        trodes_timestamps = get_Trodes_timestamps(session_paths['timestamps_dat'])
-        len_recording_sec = (trodes_timestamps[-1] - trodes_timestamps[0]) / fs
-        ch_lfp_data = readTrodesExtractedDataFile(LFP_dir + LFP_file_str.format(str(1000 * probe_i + good_cluster_channels[0])))['data']
-        lfp_fs = np.round(1 / (len_recording_sec / len(ch_lfp_data)))
-        subsample_bins = int(lfp_fs * 2 / 1000)
+            # -------------------------------------------------------- #
+            #      Export LFP data from channels of good units
+            # -------------------------------------------------------- #
+            cluster_label_df = pd.read_csv(session_paths['probe_dir'] + 'cluster_info.tsv', sep="\t")
+            good_clusters = cluster_label_df.cluster_id[cluster_label_df['group'] == 'good'].to_numpy()
+            good_cluster_channels = cluster_label_df.ch[cluster_label_df['group'] == 'good'].to_numpy()
+            assert len(good_clusters) == len(good_cluster_channels)
 
-        ch_lfp_trialwise_list = []
+            choice_df = bu.select_choice_trials_w_TTLs(joblib.load(preprocess_dir + 'behav_df'))
+            trodes_timestamps = get_Trodes_timestamps(session_paths['timestamps_dat'])
+            len_recording_sec = (trodes_timestamps[-1] - trodes_timestamps[0]) / fs
+            ch_lfp_data = readTrodesExtractedDataFile(LFP_dir + LFP_file_str.format(str(1000 * probe_i + good_cluster_channels[0])))['data']
+            lfp_fs = np.round(1 / (len_recording_sec / len(ch_lfp_data)))
+            subsample_bins = int(lfp_fs * 2 / 1000)
 
-        for chan_i in trange(len(good_clusters)):
-            chan = good_cluster_channels[chan_i]
-            ch_lfp_data = readTrodesExtractedDataFile(LFP_dir + LFP_file_str.format(str(1000 * probe_i + chan)))['data']
-            ch_lfp_data_arr = np.array([x[0] for x in ch_lfp_data])
+            ch_lfp_trialwise_list = []
 
-            ch_lfp_data_ms_arr = np.repeat(ch_lfp_data_arr, 2).reshape(-1, subsample_bins).mean(axis=1)
+            for chan_i in trange(len(good_clusters)):
+                chan = good_cluster_channels[chan_i]
+                ch_lfp_data = readTrodesExtractedDataFile(LFP_dir + LFP_file_str.format(str(1000 * probe_i + chan)))['data']
+                ch_lfp_data_arr = np.array([x[0] for x in ch_lfp_data])
 
-            # add neuron axis in order to reuse trial_start_align (can handle spiking data of all neurons at once)
-            ch_lfp_trialwise, temp_df = trace_utils.trial_start_align(choice_df, ch_lfp_data_ms_arr[np.newaxis, ...], sps=1000)
-            ch_lfp_trialwise_list.append(ch_lfp_trialwise[0])  # remove the extra neuron axis
+                ch_lfp_data_ms_arr = np.repeat(ch_lfp_data_arr, 2).reshape(-1, subsample_bins).mean(axis=1)
 
-        neuron_trialwise_lfp_mat = np.array(ch_lfp_trialwise_list)
-        assert neuron_trialwise_lfp_mat.shape[0] == len(good_clusters)
-        assert neuron_trialwise_lfp_mat.shape[1] == len(choice_df)
-        joblib.dump(neuron_trialwise_lfp_mat, lfp_output_dir + 'trialwise_start_align_lfp_mat_in_ms',compress=5)
+                # add neuron axis in order to reuse trial_start_align (can handle spiking data of all neurons at once)
+                ch_lfp_trialwise, temp_df = trace_utils.trial_start_align(choice_df, ch_lfp_data_ms_arr[np.newaxis, ...], sps=1000)
+                ch_lfp_trialwise_list.append(ch_lfp_trialwise[0])  # remove the extra neuron axis
 
-
-if DATA_OBJECT:
-    n_neurons = 0
-    for probe_i in range(1, metadata['n_probes']+1):
-        metadata['probe_num'] = probe_i
-
-        # -------------------------------------------------------- #
-        # Chop neuron activity into trials and align to trial start
-        # -------------------------------------------------------- #
-        probe_save_dir = preprocess_dir + f"probe{probe_i}/"
-        trialwise_binned_mat, choice_df = tu.align_trialwise_spike_times_to_start(preprocess_dir, probe_save_dir)
-
-        n_probe_neurons, n_trials, _ = trialwise_binned_mat.shape
-        n_neurons += n_probe_neurons
-        print('Probe', probe_i, 'has', n_probe_neurons, 'neurons with', n_trials, 'trials.')
-
-        # ------------------------------------------------------------------------- #
-        # Downsample spiking activity, create alignment traces, and save separately
-        # ------------------------------------------------------------------------- #
-        trace_utils.align_traces_to_task_events(choice_df, trialwise_binned_mat, alignment_param_dict, save_dir=probe_save_dir)
-        trace_utils.interpolate_traces(choice_df, trialwise_binned_mat, interpolation_param_dict, save_dir=probe_save_dir)
-
-        # -------------------------------------------------------- #
-        # Save datapath, behavioral and metadata to data object
-        # -------------------------------------------------------- #
-        print('Creating data object...')
-        metadata['nrn_phy_ids'] = joblib.load(probe_save_dir + f"spike_mat_in_ms.npy")['row_cluster_id']
-        data_objs.TwoAFC(probe_save_dir, choice_df, metadata).to_pickle()
-        print('---------------------------------------------------------------------------------')
+            neuron_trialwise_lfp_mat = np.array(ch_lfp_trialwise_list)
+            assert neuron_trialwise_lfp_mat.shape[0] == len(good_clusters)
+            assert neuron_trialwise_lfp_mat.shape[1] == len(choice_df)
+            joblib.dump(neuron_trialwise_lfp_mat, lfp_output_dir + 'trialwise_start_align_lfp_mat_in_ms',compress=5)
 
 
-    if WRITE_METADATA:
-        metadata['n_good_units'] = n_neurons
-        metadata['n_trials'] = n_trials
-        metadata = write_session_metadata_to_csv(metadata, 'server')
+    if DATA_OBJECT:
+        n_neurons = 0
+        for probe_i in range(1, metadata['n_probes']+1):
+            metadata['probe_num'] = probe_i
+
+            # -------------------------------------------------------- #
+            # Chop neuron activity into trials and align to trial start
+            # -------------------------------------------------------- #
+            probe_save_dir = preprocess_dir + f"probe{probe_i}/"
+            trialwise_binned_mat, choice_df = tu.align_trialwise_spike_times_to_start(preprocess_dir, probe_save_dir)
+
+            n_probe_neurons, n_trials, _ = trialwise_binned_mat.shape
+            n_neurons += n_probe_neurons
+            print('Probe', probe_i, 'has', n_probe_neurons, 'neurons with', n_trials, 'trials.')
+
+            # ------------------------------------------------------------------------- #
+            # Downsample spiking activity, create alignment traces, and save separately
+            # ------------------------------------------------------------------------- #
+            trace_utils.align_traces_to_task_events(choice_df, trialwise_binned_mat, alignment_param_dict, save_dir=probe_save_dir)
+            trace_utils.interpolate_traces(choice_df, trialwise_binned_mat, interpolation_param_dict, save_dir=probe_save_dir)
+
+            # -------------------------------------------------------- #
+            # Save datapath, behavioral and metadata to data object
+            # -------------------------------------------------------- #
+            print('Creating data object...')
+            metadata['nrn_phy_ids'] = joblib.load(probe_save_dir + f"spike_mat_in_ms.npy")['row_cluster_id']
+            data_objs.TwoAFC(probe_save_dir, choice_df, metadata).to_pickle()
+            print('---------------------------------------------------------------------------------')
+
+
+        if WRITE_METADATA:
+            metadata['n_good_units'] = n_neurons
+            metadata['n_trials'] = n_trials
+            metadata = write_session_metadata_to_csv(metadata, DATA_ROOT)
